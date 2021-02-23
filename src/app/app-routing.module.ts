61f6ebd0243556a402e271b59c04f51ed1f28f54
Nandan <nandanjha63@gmail.com>
2021-02-23 08:41:07 +0530

    profile worked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   {
     path: 'home',
-    component: HomeComponent 
+    component: HomeComponent
   },
   {
     path: 'services',
@@ -32,6 +32,7 @@ const routes: Routes = [
     path: 'about',
     component: AboutComponent
   },
+  {path:'', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
